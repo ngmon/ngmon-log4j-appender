09fb7e3a5afd5317c8513f1fe424cb5ca23c1b5a
xtovarn <xdrgithubol0>
2013-01-30 11:39:19 +0100

    optimize imports

--- src/main/java/com/mycompany/log4jproject/UnixSocketAppender.java
+++ src/main/java/com/mycompany/log4jproject/UnixSocketAppender.java
@@ -4,16 +4,6 @@
  */
 package com.mycompany.log4jproject;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.io.ObjectOutputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
 import org.apache.log4j.helpers.LogLog;
@@ -21,7 +11,11 @@ import org.apache.log4j.spi.ErrorCode;
 import org.apache.log4j.spi.LoggingEvent;
 import org.newsclub.net.unix.AFUNIXSocket;
 import org.newsclub.net.unix.AFUNIXSocketAddress;
-import org.newsclub.net.unix.AFUNIXSocketException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.io.ObjectOutputStream;
 
 /**
  *
